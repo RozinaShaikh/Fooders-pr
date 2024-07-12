8341acdb7bb1cdeaa321aa652bdd53af88018f0b
RozinaShaikh <rozina0306@gmail.com>
2024-07-13 00:10:27 +0530

    Updated code to show description

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -121,7 +121,7 @@ export const food_list = [
         name: "Greek salad",
         image: food_1,
         price: 150,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "The plate is adorned with thinly sliced rounds of asparagus , there is a bright orange sauce, providing a striking contrast to the green asparagus . The dish is lightly garnished, potentially with a sprinkle of black pepper adding a touch of texture and flavour.",
         category: "Salad"
     },
     {
@@ -129,161 +129,161 @@ export const food_list = [
         name: "Veg salad",
         image: food_2,
         price: 100,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "The salad appears to contain a variety of fresh greens, including lettuce. There is also some shaved/grated toppings, such as Parmesan cheese or fried onions, enhancing the texture and flavour. ",
         category: "Salad"
     }, {
         _id: "3",
         name: "Clover Salad",
         image: food_3,
         price: 130,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "The primary ingredient, clover leaves are tender and have a mild, slightly sweet flavour. They can be used fresh from the garden. Fresh herbs like parsley, dill, or chives are added to enhance the taste. Clover leaves are rich in vitamins A, C, and K, and provide various minerals. It contains antioxidants that are beneficial for overall health.",
         category: "Salad"
     }, {
         _id: "4",
         name: "Chicken Salad",
         image: food_4,
         price: 200,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Chicken salad is a versatile and popular dish that combines cooked chicken with a variety of ingredients and dressings. Common additions include celery, which adds crunch, and onions for a bit of bite. Other vegetables like bell peppers, carrots, and cucumbers can also be included.",
         category: "Salad"
     }, {
         _id: "5",
         name: "Lasagna Rolls",
         image: food_5,
         price: 180,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "These lasagna rolls look delicious, with a creamy and cheesy exterior, and they offer a creative and convenient way to enjoy the flavours of traditional lasagna. Each roll is made from a lasagna noodle that has been filled, rolled up, and baked. The filling appears to include ingredients like cheese and possibly spinach, given the green flecks visible inside the roll. It is topped with melted cheese, which has turned golden and bubbly from baking.",
         category: "Rolls"
     }, {
         _id: "6",
         name: "Peri Peri Rolls",
         image: food_6,
         price: 140,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Peri peri rolls are a flavourful and spicy dish inspired by the popular Peri Peri (or Piri Piri) sauce, which originates from African and Portuguese cuisines. These rolls typically feature a combination of spicy chicken, fresh vegetables, and a variety of sauces, all wrapped in a bread roll.",
         category: "Rolls"
     }, {
         _id: "7",
         name: "Chicken Rolls",
         image: food_7,
         price: 200,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description:"Originating from Middle Eastern cuisine, these rolls consist of thinly sliced or shredded chicken, often marinated with spices like cumin, paprika, and garlic, wrapped in flatbread (like pita or lavash) along with vegetables and a variety of sauces.",
         category: "Rolls"
     }, {
         _id: "8",
         name: "Veg Rolls",
         image: food_8,
         price: 150,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description:"Originating from Indian cuisine, these rolls feature a filling of sautéed vegetables such as bell peppers, onions, and potatoes, flavoured with spices like cumin, coriander, and turmeric. They are rolled in a paratha (Indian flatbread) or a tortilla along with sauces like mint chutney or tamarind sauce. They are often enjoyed for their combination of flavours, textures, and sometimes the convenience of being handheld and portable.",
         category: "Rolls"
     }, {
         _id: "9",
         name: "Ripple Ice Cream",
         image: food_9,
         price: 120,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description:"Ripple ice cream  refers to a type of ice cream where a flavoured sauce or ripple is swirled into a base ice cream. It offers a delightful combination of creamy base flavours complemented by swirls of indulgent sauces or syrups, making it a favourite for those who enjoy a bit of variety and richness in their ice cream experience.",
         category: "Deserts"
     }, {
         _id: "10",
         name: "Fruit Ice Cream",
         image: food_10,
         price: 140,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description:"Fruit ice cream refers to a type of ice cream that incorporates real fruits into its base, resulting in a refreshing and fruity flavour profile. Common fruits used include strawberries, mangoes, peaches, bananas, raspberries, and blueberries.",
         category: "Deserts"
     }, {
         _id: "11",
         name: "Jar Ice Cream",
         image: food_11,
         price: 90,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description:"Jar ice cream is a popular DIY treat for its simplicity and the ability to tailor flavours to personal preferences. It's often garnished with additional toppings like whipped cream, sprinkles, or fresh fruit for added flavour and texture.",
         category: "Deserts"
     }, {
         _id: "12",
         name: "Vanilla Ice Cream",
         image: food_12,
         price: 100,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Vanilla ice cream is cherished for its simplicity, creamy consistency, and timeless appeal. It serves as a versatile base for countless dessert creations while remaining delightful on its own. It is often served in bowls or cones, sometimes garnished with a sprig of mint or a dusting of powdered sugar for presentation. It's a staple flavour that appeals to both children and adults for its comforting taste and creamy texture.",
         category: "Deserts"
     }, {
         _id: "13",
         name: "Chicken Sandwich",
         image: food_13,
         price: 250,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "A chicken sandwich is a popular dish consisting of chicken meat served between two slices of bread or in a roll. Roasted chicken meat is shredded or pulled apart, often mixed with barbecue sauce or other seasonings and mixed with some veggies it is added to the bread.",
         category: "Sandwich"
     }, {
         _id: "14",
         name: "Vegan Sandwich",
         image: food_14,
         price: 180,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Vegan sandwiches are appreciated for their creativity, variety, and health benefits, offering a delicious and satisfying option for those following a plant-based diet. Vegan sandwiches often start with a base of bread. Instead of animal-based proteins like meat or cheese fresh vegetables add crunch, flavour, and nutrition.",
         category: "Sandwich"
     }, {
         _id: "15",
         name: "Grilled Sandwich",
         image: food_15,
         price: 160,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "A grilled sandwich, also known as a toasted sandwich or panini, is a type of sandwich that is typically heated and pressed on a grill or panini press. Grilled sandwiches are appreciated for their crispy exterior, melted cheese, and warm fillings, making them a comforting and versatile option for a quick meal or snack.",
         category: "Sandwich"
     }, {
         _id: "16",
         name: "Bread Sandwich",
         image: food_16,
         price: 150,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "A bread sandwich, often considered one of the simplest and most basic types of sandwiches, consists primarily of slices of bread with minimal additional ingredients.",
         category: "Sandwich"
     }, {
         _id: "17",
         name: "Cup Cake",
         image: food_17,
         price: 60,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Cupcakes are often topped with frosting, which adds sweetness and enhances the flavour. cupcakes are loved for their variety of flavours, ease of serving, and ability to be customized with different frostings and decorations. They are a delightful treat enjoyed by people of all ages for their sweetness and charm.",
         category: "Cake"
     }, {
         _id: "18",
-        name: "Vegan Cake",
+        name: "Vegan Strawberry Cake",
         image: food_18,
         price: 100,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "A vegan strawberry cake is a delightful dessert that omits any animal-derived ingredients, making it suitable for those following a vegan diet. Vegan strawberry cake offers a delicious alternative to traditional cakes, maintaining a light and moist texture while celebrating the sweetness and natural flavours of strawberries without the use of dairy or eggs.",
         category: "Cake"
     }, {
         _id: "19",
         name: "Butterscotch Cake",
         image: food_19,
         price: 200,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "A butterscotch cake is a rich and indulgent dessert featuring the flavours of brown sugar, butter, and often caramel. Butterscotch cake is loved for its caramel-like flavour, moist texture, and luxurious frosting, making it a delightful treat for those who enjoy sweet and indulgent desserts.",
         category: "Cake"
     }, {
         _id: "20",
-        name: "Sliced Cake",
+        name: "Sliced Cheesecake",
         image: food_20,
         price: 90,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Cheesecake is beloved for its creamy texture, rich flavour, and versatility in flavour combinations and toppings. It's a popular dessert choice for celebrations and special occasions, offering a decadent treat that satisfies both sweet tooth cravings and appreciation for creamy desserts.",
         category: "Cake"
     }, {
         _id: "21",
         name: "Garlic Mushroom ",
         image: food_21,
         price: 140,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Garlic mushrooms are a delicious and savory dish featuring mushrooms cooked with garlic and often complemented by herbs and spices. Garlic mushrooms are appreciated for their simplicity, versatility, and rich, savory flavours. They make a delicious addition to any meal and are especially enjoyed by mushroom enthusiasts and garlic lovers alike.",
         category: "Pure Veg"
     }, {
         _id: "22",
         name: "Fried Cauliflower",
         image: food_22,
         price: 120,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Fried cauliflower is a tasty and versatile dish where cauliflower florets are coated and fried until crispy. It is seasoned with some parsley, coriander or fruits like pomegranate. Fried cauliflower is appreciated for its crispy texture and versatility. It offers a delightful alternative to traditional fried foods and is a popular choice for those looking to enjoy cauliflower in a flavourful and satisfying way.",
         category: "Pure Veg"
     }, {
         _id: "23",
         name: "Mix Veg Pulao",
         image: food_23,
         price: 200,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Mix vegetable pulao is a flavourful and aromatic Indian rice dish made with a variety of vegetables and fragrant spices. It is typically served hot, garnished with fresh cilantro or mint leaves. Mix vegetable pulao is cherished for its aromatic spices, vibrant colours from the vegetables, and the fragrant basmati rice. It's a popular dish in Indian cuisine that offers a balanced and nutritious meal with a delightful blend of flavours.",
         category: "Pure Veg"
     }, {
         _id: "24",
         name: "Rice Zucchini",
         image: food_24,
         price: 180,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Rice with zucchini is a delicious and nutritious dish that combines tender zucchini with fluffy rice, often enhanced with aromatic herbs and spices. It is appreciated for its simplicity, versatility, and the combination of textures and flavours from the tender zucchini and fluffy rice. It's a wholesome dish that's both satisfying and easy to prepare for a variety of meals.",
         category: "Pure Veg"
     },
     {
@@ -291,7 +291,7 @@ export const food_list = [
         name: "Cheese Pasta",
         image: food_25,
         price: 140,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Cheese pasta, also known as macaroni and cheese or simply mac 'n' cheese, is a comforting and popular dish made with cooked pasta and a creamy cheese sauce. It's a classic comfort food that appeals to both kids and adults alike, perfect for a cozy dinner or as a crowd-pleasing dish for gatherings and potlucks.",
         category: "Pasta"
     },
     {
@@ -299,49 +299,49 @@ export const food_list = [
         name: "Tomato Pasta",
         image: food_26,
         price: 180,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Tomato pasta, also known as pasta pomodoro or pasta with tomato sauce, is a delicious and versatile dish that highlights the flavours of fresh tomatoes and herbs. A classic Italian-inspired dish that's perfect for weeknight dinners or special occasions, offering a comforting and satisfying meal for pasta lovers everywhere.",
         category: "Pasta"
     }, {
         _id: "27",
         name: "Creamy Pasta",
         image: food_27,
         price: 200,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Creamy pasta refers to a dish where pasta is coated in a rich, creamy sauce typically made from dairy products such as cream, cheese, or butter. Creamy pasta is typically served hot, garnished with additional grated cheese, freshly ground black pepper, and chopped herbs like parsley or basil.",
         category: "Pasta"
     }, {
         _id: "28",
         name: "Chicken Pasta",
         image: food_28,
         price: 250,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Chicken pasta is a hearty and flavourful dish that combines tender pieces of chicken with pasta and a variety of complementary ingredients. Chicken pasta dishes are loved for their versatility, as they can be customized with different sauces and vegetables to suit personal tastes and dietary preferences.",
         category: "Pasta"
     }, {
         _id: "29",
         name: "Buttter Noodles",
         image: food_29,
         price: 180,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Butter noodles are appreciated for their simplicity, versatility, and quick preparation time. They provide a satisfying and comforting meal option that can be easily customized with different seasonings and additions to suit personal tastes.",
         category: "Noodles"
     }, {
         _id: "30",
         name: "Veg Noodles",
         image: food_30,
         price: 160,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Veg noodles, also known as vegetable noodles or vegetable stir-fried noodles, are a popular and versatile dish made with stir-fried vegetables and noodles. They are a popular choice for quick and flavourful meals, offering a satisfying balance of carbohydrates, vegetables, and often protein in one dish.",
         category: "Noodles"
     }, {
         _id: "31",
         name: "Somen Noodles",
         image: food_31,
         price: 150,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Somen noodles are thin Japanese noodles made from wheat flour and typically served cold, making them a refreshing and light dish, especially popular during hot summer months. They are prized for their delicate texture, simplicity, and ability to showcase the flavours of the accompanying dipping sauce and toppings.",
         category: "Noodles"
     }, {
         _id: "32",
         name: "Cooked Noodles",
         image: food_32,
         price: 140,
-        description: "Food provides essential nutrients for overall health and well-being",
+        description: "Cooked noodles refer to noodles that have been boiled or otherwise prepared until they reach a soft and edible texture. The term cooked noodles encompasses a wide variety of noodles from different cultures and culinary traditions, each with its own characteristics and uses.",
         category: "Noodles"
     }
 ]
